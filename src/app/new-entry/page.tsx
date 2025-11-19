0b70a936743d93b22ca71a6800f6a3c74b7fd21c
RadekKosiada <kosiadar@gmail.com>
2025-11-19 21:33:46 +0100

    delete test client component

--- src/app/new-entry/page.tsx
+++ src/app/new-entry/page.tsx
@@ -1,11 +1,11 @@
-import LikeButton from "@/ui/button/like-button";
+
 import ClientNewEntry from "./ClientNewEntry";
 
 export default function NewEntry() {
     return (
         <>
-            <ClientNewEntry />;
-            <LikeButton likes={10} />
+            <ClientNewEntry />
+
         </>
     )
 
--- src/ui/button/like-button.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function LikeButton({ likes }: { likes: number }) {
-    return <p>{likes}</p>
-}
