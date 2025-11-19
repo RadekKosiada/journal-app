8fb4e953b54a578ec7b9e5567bafd037abbcdb8c
RadekKosiada <kosiadar@gmail.com>
2025-11-19 20:55:37 +0100

    add new entry page

---
+++ src/app/new-entry/page.tsx
@@ -0,0 +1,3 @@
+export default function NewEntry() {
+    return <h1>This is where you can add a new entry</h1>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <h1>this is Home Page</h1>
+        <>
+            <h1>this is Home Page</h1>
+            <Link href="/new-entry">Go to New Entry Page</Link>
+        </>
+
     );
 }
