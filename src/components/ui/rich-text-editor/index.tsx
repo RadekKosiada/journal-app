18463bfc774181b2ac0bf2b01757449d6e0e3048
RadekKosiada <kosiadar@gmail.com>
2025-11-23 21:17:50 +0100

    add scss for rte editor

--- src/components/ui/rich-text-editor/index.tsx
+++ src/components/ui/rich-text-editor/index.tsx
@@ -1,17 +1,23 @@
-'use client'
+'use client';
+import styles from "./rich-text-editor.module.scss";
 
-import { EditorContent, useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import MenuBar from './menu-bar';
 
 const Tiptap = () => {
     const editor = useEditor({
         extensions: [StarterKit],
         content: "Add your new entry here...",
-        immediatelyRender: false,
+        immediatelyRender: false
     })
 
-    return <EditorContent editor={editor} />
+    return (
+        <>
+            <MenuBar editor={editor} />
+            <EditorContent editor={editor} className={styles.rteEditor} />
+        </>
+    )
 }
 
-
-export default Tiptap
+export default Tiptap;
