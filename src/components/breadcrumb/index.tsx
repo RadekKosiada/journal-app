020e745ab7f790cf5b676a011e21e1d2ecba70c0
RadekKosiada <kosiadar@gmail.com>
2026-01-25 20:42:45 +0100

    create Breadcrumb component

---
+++ src/components/breadcrumb/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { cn } from "@/lib/utils";
+import { BreadcrumbDataType } from "@/types/BreadcrumbDataType";
+import { ButtonLink } from "../ui/link";
+import styles from "./breadcrumb.module.scss";
+
+
+const Breadcrumb = ({ breadcrumbData }: { breadcrumbData: BreadcrumbDataType }) => {
+
+    return (
+        <nav aria-label="breadcrumbs" className={styles.breadcrumb}>
+
+            {/* pass props urls and pages' names and iterate with each */}
+            < ol className={styles.breadcrumbList} >
+                {/* check if breadcrumbData is an array */}
+                {breadcrumbData instanceof Array && breadcrumbData.map((item, index) => (
+
+                    <li key={index} className={cn(item.current && styles.breadcrumbListItemCurrent, styles.breadcrumbListItem)}>
+                        <ButtonLink variant="link" href={item.url}>{item.label}</ButtonLink>
+                    </li>
+                ))}
+            </ol >
+        </nav >
+    )
+}
+export default Breadcrumb;
---
+++ src/types/BreadcrumbDataType.ts
@@ -0,0 +1,9 @@
+
+export interface BreadcrumbProperties {
+    order: number;
+    current: boolean;
+    url: string;
+    label: string;
+}
+
+export type BreadcrumbDataType = BreadcrumbProperties[];
