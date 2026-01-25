311900dded10e5b4a888a17bf2f71b28c6ce3894
RadekKosiada <kosiadar@gmail.com>
2026-01-25 21:58:56 +0100

    add Header component to ClientNewEntry for improved navigation

--- src/app/new-entry/client-new-entry.tsx
+++ src/app/new-entry/client-new-entry.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Header from "@/components/header";
+
 export default function ClientNewEntry() {
     return (
         <>
+            <Header />
             <h1>This is where you can add a new entry</h1>
         </>
     );
