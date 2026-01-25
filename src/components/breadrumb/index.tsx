25d68015b154f444523194d1791ea2d0f013c335
RadekKosiada <kosiadar@gmail.com>
2026-01-25 22:16:15 +0100

    hide chevron on last element in breadcrumb

--- src/components/breadrumb/index.tsx
+++ src/components/breadrumb/index.tsx
@@ -6,21 +6,23 @@ import {
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
 import { BreadcrumbDataType } from "@/types/BreadcrumbDataType";
-
+import styles from "./breadcrumb.module.scss";
 
 export function BreadcrumbBasic({ breadcrumbData }: { breadcrumbData: BreadcrumbDataType }) {
     return (
-        <Breadcrumb>
-            <BreadcrumbList>
-                {breadcrumbData instanceof Array && breadcrumbData.map((item, index) => (
-                    <BreadcrumbItem key={index}>
-                        <BreadcrumbLink href={item.url}>{item.label}</BreadcrumbLink>
-                        <BreadcrumbSeparator />
-                    </BreadcrumbItem>
-                ))}
+        <Breadcrumb className={styles.breadcrumb}>
+            <BreadcrumbList className={styles.breadcrumbList}>
+                {
+                    breadcrumbData instanceof Array && breadcrumbData.map((item, index) => (
+                        <BreadcrumbItem key={index} className={styles.breadcrumbItem}>
+                            <BreadcrumbLink href={item.url} className={styles.breadcrumbLink}>{item.label}</BreadcrumbLink>
+                            <BreadcrumbSeparator className={styles.BreadcrumbSeparator} />
+                        </BreadcrumbItem>
+                    ))
+                }
 
-            </BreadcrumbList>
-        </Breadcrumb>
+            </BreadcrumbList >
+        </Breadcrumb >
     )
 }
 export default BreadcrumbBasic;
