6d698893bdce62857564cf4f10ec6aa281122273
RadekKosiada <kosiadar@gmail.com>
2026-01-31 22:48:42 +0100

    delete ButtonLink from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 import Header from "@/components/header";
-import { ButtonLink } from "@/components/ui/link";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <h1>this is Home Page</h1>
-            <ButtonLink variant="link" href="/new-entry">Go to New Entry Page</ButtonLink>
+            <h1>this is Home Page&#39;s Headline</h1>
         </>
 
     );
-}
-
-{/* <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-
-                        <NavigationMenuLink>Link</NavigationMenuLink>
-
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu> */}
+};
