5014469d2ce3a67e07595349a5a2d8509ac2b47c
RadekKosiada <kosiadar@gmail.com>
2026-02-07 20:36:14 +0100

    add Controller login component

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
 export function LoginForm() {
@@ -32,7 +32,7 @@ export function LoginForm() {
 
     return (
         <div className="w-full max-w-md">
-            <form {...form} onSubmit={form.handleSubmit(onSubmit)}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 {error && <p className="text-red-500">{error}</p>}
                 <FieldGroup>
                     <FieldSet>
@@ -41,22 +41,31 @@ export function LoginForm() {
                             Enter your data to login to your account
                         </FieldDescription>
                         <FieldGroup>
+                            <Controller
+                                name="email"
+                                control={form.control}
+                                render={({ field }) => (
+                                    <Field>
+                                        <FieldLabel htmlFor="email">Email</FieldLabel>
+                                        <FieldDescription>
+                                            Enter your e-mail address.
+                                        </FieldDescription>
+                                        <Input id="email" type="email" autoComplete="false" placeholder="your email" {...field} />
+                                    </Field>
+                                )}
+                            />
+                            <Controller
+                                name="password"
+                                control={form.control}
+                                render={({ field }) => (<Field>
+                                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                                    <FieldDescription>
+                                        Must be at least 8 characters long.</FieldDescription>
+                                    <Input id="password" type="password" autoComplete="false" placeholder="••••••••" {...field} />
+                                </Field>
+                                )}
+                            />
 
-                            <Field>
-                                <FieldLabel htmlFor="email">Email</FieldLabel>
-                                <FieldDescription>
-                                    Enter your e-mail address.
-                                </FieldDescription>
-                                <Input id="email" type="email" autoComplete="false" placeholder="your email" />
-                            </Field>
-
-                            <Field>
-                                <FieldLabel htmlFor="password">Password</FieldLabel>
-                                <FieldDescription>
-                                    Must be at least 8 characters long.
-                                </FieldDescription>
-                                <Input id="password" type="password" autoComplete="false" placeholder="••••••••" />
-                            </Field>
                         </FieldGroup>
                     </FieldSet>
                     <Field orientation="horizontal">
