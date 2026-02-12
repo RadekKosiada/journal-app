f3391a115546620992d28f74886b812acbb8114b
RadekKosiada <kosiadar@gmail.com>
2026-02-12 21:12:54 +0100

    import schema to db/index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@libsql/client";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema/user_data";
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema });
